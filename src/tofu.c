c0a07e1aedd4d154938edb506f49db8db92ed6db
Michael Gene Brockus <michaelbrockus@gmail.com>
2023-10-09 14:53:50 -0600

    included math

--- src/tofu.c
+++ src/tofu.c
@@ -8,6 +8,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 // Function to create a new TriloTofu instance from an integer
 TriloTofu trilo_xdata_tofu_create_from_integer(int value) {
