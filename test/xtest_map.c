a1f4a243262aa42122d788c2005723d17c930794
Michael Gene Brockus <michaelbrockus@gmail.com>
2023-11-05 12:21:18 -0600

    update map test

--- test/xtest_map.c
+++ test/xtest_map.c
@@ -41,7 +41,7 @@
 // Test case 1: Test TriloMap creation and destruction
 XTEST_CASE(xdata_let_map_create_and_destroy) {
     TriloMap* map = trilo_xdata_map_create(INTEGER_TYPE);
-    TEST_ASSERT_NOT_NULL(map);
+    TEST_ASSERT_NOT_NULL_PTR(map);
 
     trilo_xdata_map_destroy(map);
 }
@@ -49,16 +49,16 @@ XTEST_CASE(xdata_let_map_create_and_destroy) {
 // Test case 2: Test TriloMap insert and search
 XTEST_CASE(xdata_let_map_insert_and_search) {
     TriloMap* map = trilo_xdata_map_create(STRING_TYPE);
-    TEST_ASSERT_NOT_NULL(map);
+    TEST_ASSERT_NOT_NULL_PTR(map);
 
     TriloTofu key = trilo_xdata_tofu_create_from_string("name");
     TriloTofu value = trilo_xdata_tofu_create_from_string("John");
 
-    TriloMapError insertResult = trilo_xdata_map_insert(map, key, value);
-    TEST_ASSERT_EQUAL(trilo_xdata_map_SUCCESS, insertResult);
+    TofuError insertResult = trilo_xdata_map_insert(map, key, value);
+    TEST_ASSERT_EQUAL_ENUM(TRILO_XDATA_TYPE_SUCCESS, insertResult);
 
-    TriloMapError searchResult = trilo_xdata_map_search(map, key);
-    TEST_ASSERT_EQUAL(trilo_xdata_map_SUCCESS, searchResult);
+    TofuError searchResult = trilo_xdata_map_search(map, key);
+    TEST_ASSERT_EQUAL_ENUM(TRILO_XDATA_TYPE_SUCCESS, searchResult);
 
     // Clean up
     trilo_xdata_map_destroy(map);
@@ -67,19 +67,19 @@ XTEST_CASE(xdata_let_map_insert_and_search) {
 // Test case 3: Test TriloMap remove operation
 XTEST_CASE(xdata_let_map_remove) {
     TriloMap* map = trilo_xdata_map_create(INTEGER_TYPE);
-    TEST_ASSERT_NOT_NULL(map);
+    TEST_ASSERT_NOT_NULL_PTR(map);
 
     TriloTofu key = trilo_xdata_tofu_create_from_string("age");
     TriloTofu value = trilo_xdata_tofu_create_from_integer(30);
 
-    TriloMapError insertResult = trilo_xdata_map_insert(map, key, value);
-    TEST_ASSERT_EQUAL(trilo_xdata_map_SUCCESS, insertResult);
+    TofuError insertResult = trilo_xdata_map_insert(map, key, value);
+    TEST_ASSERT_EQUAL_ENUM(TRILO_XDATA_TYPE_SUCCESS, insertResult);
 
-    TriloMapError removeResult = trilo_xdata_map_remove(map, key);
-    TEST_ASSERT_EQUAL(trilo_xdata_map_SUCCESS, removeResult);
+    TofuError removeResult = trilo_xdata_map_remove(map, key);
+    TEST_ASSERT_EQUAL_ENUM(TRILO_XDATA_TYPE_SUCCESS, removeResult);
 
-    TriloMapError searchResult = trilo_xdata_map_search(map, key);
-    TEST_ASSERT_EQUAL(trilo_xdata_map_NOT_FOUND, searchResult);
+    TofuError searchResult = trilo_xdata_map_search(map, key);
+    TEST_ASSERT_EQUAL_ENUM(TRILO_XDATA_TYPE_WAS_UNKNOWN, searchResult);
 
     // Clean up
     trilo_xdata_map_destroy(map);
@@ -88,21 +88,21 @@ XTEST_CASE(xdata_let_map_remove) {
 // Test case 4: Test TriloMap setter and getter operations
 XTEST_CASE(xdata_let_map_getter_and_setter) {
     TriloMap* map = trilo_xdata_map_create(BOOLEAN_TYPE);
-    TEST_ASSERT_NOT_NULL(map);
+    TEST_ASSERT_NOT_NULL_PTR(map);
 
     TriloTofu key = trilo_xdata_tofu_create_from_string("is_valid");
     TriloTofu value = trilo_xdata_tofu_create_from_boolean(true);
 
-    TriloMapError insertResult = trilo_xdata_map_insert(map, key, value);
-    TEST_ASSERT_EQUAL(trilo_xdata_map_SUCCESS, insertResult);
+    TofuError insertResult = trilo_xdata_map_insert(map, key, value);
+    TEST_ASSERT_EQUAL_ENUM(TRILO_XDATA_TYPE_SUCCESS, insertResult);
 
     TriloTofu newValue = trilo_xdata_tofu_create_from_boolean(false);
-    TriloMapError setResult = trilo_xdata_map_set(map, key, newValue);
-    TEST_ASSERT_EQUAL(trilo_xdata_map_SUCCESS, setResult);
+    TofuError setResult = trilo_xdata_map_set(map, key, newValue);
+    TEST_ASSERT_EQUAL_ENUM(TRILO_XDATA_TYPE_SUCCESS, setResult);
 
     TriloTofu retrievedValue;
-    TriloMapError getResult = trilo_xdata_map_get(map, key, &retrievedValue);
-    TEST_ASSERT_EQUAL(trilo_xdata_map_SUCCESS, getResult);
+    TofuError getResult = trilo_xdata_map_get(map, key, &retrievedValue);
+    TEST_ASSERT_EQUAL_ENUM(TRILO_XDATA_TYPE_SUCCESS, getResult);
     TEST_ASSERT_TRUE(trilo_xdata_tofu_get_boolean(retrievedValue) == false);
 
     // Clean up
@@ -112,21 +112,21 @@ XTEST_CASE(xdata_let_map_getter_and_setter) {
 // Test case 5: Test TriloMap size operation
 XTEST_CASE(xdata_let_map_size) {
     TriloMap* map = trilo_xdata_map_create(DOUBLE_TYPE);
-    TEST_ASSERT_NOT_NULL(map);
+    TEST_ASSERT_NOT_NULL_PTR(map);
 
-    TEST_ASSERT_EQUAL(0, trilo_xdata_map_size(map));
+    TEST_ASSERT_EQUAL_ENUM(0, trilo_xdata_map_size(map));
 
     TriloTofu key1 = trilo_xdata_tofu_create_from_string("price");
     TriloTofu value1 = trilo_xdata_tofu_create_from_double(19.99);
     trilo_xdata_map_insert(map, key1, value1);
 
-    TEST_ASSERT_EQUAL(1, trilo_xdata_map_size(map));
+    TEST_ASSERT_EQUAL_ENUM(1, trilo_xdata_map_size(map));
 
     TriloTofu key2 = trilo_xdata_tofu_create_from_string("quantity");
     TriloTofu value2 = trilo_xdata_tofu_create_from_integer(5);
     trilo_xdata_map_insert(map, key2, value2);
 
-    TEST_ASSERT_EQUAL(2, trilo_xdata_map_size(map));
+    TEST_ASSERT_EQUAL_ENUM(2, trilo_xdata_map_size(map));
 
     // Clean up
     trilo_xdata_map_destroy(map);
@@ -135,14 +135,14 @@ XTEST_CASE(xdata_let_map_size) {
 // Test case 6: Test TriloMap contains operation
 XTEST_CASE(xdata_let_map_contains) {
     TriloMap* map = trilo_xdata_map_create(CHAR_TYPE);
-    TEST_ASSERT_NOT_NULL(map);
+    TEST_ASSERT_NOT_NULL_PTR(map);
 
     TriloTofu key = trilo_xdata_tofu_create_from_char('A');
     TriloTofu value = trilo_xdata_tofu_create_from_string("Apple");
     trilo_xdata_map_insert(map, key, value);
 
     TEST_ASSERT_TRUE(trilo_xdata_map_contains(map, key));
-    TEST_ASSERT_FALSE(trilo_xdata_map_contains(map, trilo_xdata_tofu_create_from_char('B'));
+    TEST_ASSERT_FALSE(trilo_xdata_map_contains(map, trilo_xdata_tofu_create_from_char('B')));
 
     // Clean up
     trilo_xdata_map_destroy(map);
