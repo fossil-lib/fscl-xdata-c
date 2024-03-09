7c19c054e76fd17ef894f5b77bc6e8f7341ebc15
Michael Gene Brockus (Dreamer) <55331536+dreamer-coding-555@users.noreply.github.com>
2024-03-09 16:43:01 -0600

    Update stack.c

--- code/source/stack.c
+++ code/source/stack.c
@@ -10,7 +10,7 @@ Description:
     feel free to contact Michael at michaelbrockus@gmail.com.
 ==============================================================================
 */
-#include "fossil/xdata/stack.h"
+#include "fossil/xstructures/stack.h"
 #include <stdio.h>
 #include <stdlib.h>
 
