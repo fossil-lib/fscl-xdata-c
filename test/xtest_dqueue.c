c3a6e94fef1bed7c1eed1103fb9b9a07c449ad47
Michael Gene Brockus (Dreamer) <55331536+dreamer-coding-555@users.noreply.github.com>
2024-03-09 16:43:51 -0600

    Update xtest_dqueue.c

--- test/xtest_dqueue.c
+++ test/xtest_dqueue.c
@@ -10,7 +10,7 @@ Description:
     feel free to contact Michael at michaelbrockus@gmail.com.
 ==============================================================================
 */
-#include "fossil/xdata/dqueue.h" // lib source code
+#include "fossil/xstructures/dqueue.h" // lib source code
 
 #include <fossil/xtest.h>   // basic test tools
 #include <fossil/xassert.h> // extra asserts
