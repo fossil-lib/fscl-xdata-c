372e566a017d050f223cde2b17ee130fe7770fea
Michael Gene Brockus (Dreamer) <55331536+dreamer-coding-555@users.noreply.github.com>
2024-03-09 16:45:06 -0600

    Update xtest_stack.c

--- test/xtest_stack.c
+++ test/xtest_stack.c
@@ -10,7 +10,7 @@ Description:
     feel free to contact Michael at michaelbrockus@gmail.com.
 ==============================================================================
 */
-#include "fossil/xdata/stack.h" // lib source code
+#include "fossil/xstructures/stack.h" // lib source code
 
 #include <fossil/xtest.h>   // basic test tools
 #include <fossil/xassert.h> // extra asserts
