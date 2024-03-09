beee27e51bcfa05dee966798c5314f7bb878b521
Michael Gene Brockus (Dreamer) <55331536+dreamer-coding-555@users.noreply.github.com>
2024-03-09 17:16:42 -0600

    Update queue.h

--- code/include/fossil/xstructures/queue.h
+++ code/include/fossil/xstructures/queue.h
@@ -36,25 +36,110 @@ typedef struct cqueue {
 // =======================
 // CREATE and DELETE
 // =======================
+/**
+ * Create a new queue with the specified data type.
+ *
+ * @param list_type The type of data the queue will store.
+ * @return          The created queue.
+ */
 cqueue* fscl_queue_create(ctofu_type list_type);
+
+/**
+ * Erase the contents of the queue and free allocated memory.
+ *
+ * @param queue The queue to erase.
+ */
 void fscl_queue_erase(cqueue* queue);
 
 // =======================
 // ALGORITHM FUNCTIONS
 // =======================
+/**
+ * Insert data into the queue.
+ *
+ * @param queue The queue to insert data into.
+ * @param data  The data to insert.
+ * @return      The error code indicating the success or failure of the operation.
+ */
 ctofu_error fscl_queue_insert(cqueue* queue, ctofu data);
+
+/**
+ * Remove data from the queue.
+ *
+ * @param queue The queue to remove data from.
+ * @param data  The data to remove.
+ * @return      The error code indicating the success or failure of the operation.
+ */
 ctofu_error fscl_queue_remove(cqueue* queue, ctofu* data);
+
+/**
+ * Search for data in the queue.
+ *
+ * @param queue The queue to search.
+ * @param data  The data to search for.
+ * @return      The error code indicating the success or failure of the operation.
+ */
 ctofu_error fscl_queue_search(const cqueue* queue, ctofu data);
 
 // =======================
 // UTILITY FUNCTIONS
 // =======================
+/**
+ * Get the size of the queue.
+ *
+ * @param queue The queue for which to get the size.
+ * @return      The size of the queue.
+ */
 size_t fscl_queue_size(const cqueue* queue);
+
+/**
+ * Get the data from the queue matching the specified data.
+ *
+ * @param queue The queue from which to get the data.
+ * @param data  The data to search for.
+ * @return      A pointer to the matching data, or NULL if not found.
+ */
 ctofu* fscl_queue_getter(cqueue* queue, ctofu data);
+
+/**
+ * Set data in the queue.
+ *
+ * @param queue The queue in which to set the data.
+ * @param data  The data to set.
+ * @return      The error code indicating the success or failure of the operation.
+ */
 ctofu_error fscl_queue_setter(cqueue* queue, ctofu data);
+
+/**
+ * Check if the queue is not empty.
+ *
+ * @param queue The queue to check.
+ * @return      True if the queue is not empty, false otherwise.
+ */
 bool fscl_queue_not_empty(const cqueue* queue);
+
+/**
+ * Check if the queue is not a null pointer.
+ *
+ * @param queue The queue to check.
+ * @return      True if the queue is not a null pointer, false otherwise.
+ */
 bool fscl_queue_not_cnullptr(const cqueue* queue);
+
+/**
+ * Check if the queue is empty.
+ *
+ * @param queue The queue to check.
+ * @return      True if the queue is empty, false otherwise.
+ */
 bool fscl_queue_is_empty(const cqueue* queue);
+
+/**
+ * Check if the queue is a null pointer.
+ *
+ * @param queue The queue to check.
+ * @return      True if the queue is a null pointer, false otherwise.
+ */
 bool fscl_queue_is_cnullptr(const cqueue* queue);
 
 #ifdef __cplusplus
